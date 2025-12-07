2d483c93067cfa49d70397d38cf1e9a30221e3ed
joonhyong <dbshwns0528@naver.com>
2025-12-08 01:43:30 +0900

    FEAT: Navbar의 로고 클릭시 메인화면으로 이동

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,41 +2,20 @@
 
 import FitLogLogo from "@/components/ui/FitLogLogo";
 import UserIcon from "@/components/ui/UserIcon";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     /* 네비바 색상 - #FFFFFF */
-    // <nav
-    //   className="
-    //     h-[72px]
-    //     w-full
-    //     flex
-    //     items-center
-    //     justify-between
-    //     px-6
-    //     bg-white
-    //     shadow-sm
-    //   "
-    // >
+    // <nav className="h-[72px] w-full flex items-center justify-between px-8 bg-white shadow-sm">
 
     /* 네비바 색상 - Primary Color */
-    <nav
-      className="
-        h-[72px] 
-        w-full 
-        flex 
-        items-center 
-        justify-between 
-        px-8
-        bg-[#FCEDEE]
-        shadow-sm
-      "
-    >
+    <nav className="h-[72px] w-full flex items-center justify-between px-8 bg-[#FCEDEE] shadow-sm">
       {/* 왼쪽 - FitLog 로고 */}
-      <section className="flex justify-center items-center">
+      <Link href="/" className="flex items-center gap-4">
         <FitLogLogo size={44} />
-        <p className="text-fitlog-500 pl-5 text-2xl font-extrabold">FitLog</p>
-      </section>
+        <p className="text-fitlog-500 text-2xl font-extrabold">FitLog</p>
+      </Link>
 
       {/* 오른쪽 - 유저 아이콘 */}
       <section>
--- src/components/ui/UserIcon.tsx
+++ src/components/ui/UserIcon.tsx
@@ -5,15 +5,7 @@ import { User } from "lucide-react";
 export default function UserIcon() {
   return (
     <button
-      className="
-        p-1
-        border-2
-        border-gray-600
-        rounded-full 
-        hover:bg-gray-100 
-        transition 
-        flex items-center justify-center
-      "
+      className="p-1 border-2 border-gray-600 rounded-full hover:bg-gray-100 transition flex items-center justify-center"
       aria-label="user menu"
     >
       <User className="w-6 h-6 text-gray-600" />
