54d94cc5a0fdab419247ba2f8658c00064f8047e
joonhyong <dbshwns0528@naver.com>
2025-12-10 01:41:41 +0900

    FIX: 네비바 테일윈드 클래스 작성 방식  수정

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,7 @@ import UserIcon from "../ui/UserIcon";
 
 export default function Navbar() {
   return (
-    /* 네비바 색상 - #FFFFFF */
-    // <nav className="h-[72px] w-full flex items-center justify-between px-8 bg-white shadow-sm">
-
-    /* 네비바 색상 - Primary Color */
-    <nav className="h-[72px] w-full flex items-center justify-between px-8 bg-[#FCEDEE] shadow-sm">
+    <nav className="h-18 w-full flex items-center justify-between px-8 bg-fitlog-50 shadow-sm">
       {/* 왼쪽 - FitLog 로고 */}
       <Link href="/" className="flex items-center gap-4">
         <FitLogLogo size={44} />
@@ -18,7 +14,6 @@ export default function Navbar() {
       </Link>
 
       {/* 오른쪽 - 유저 아이콘 */}
-      {/* <UserProfilePopover /> */}
       <UserIcon/>
     </nav>
   );
