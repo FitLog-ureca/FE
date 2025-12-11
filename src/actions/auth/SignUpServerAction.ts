def5fa5ef826b9d5690234e0180924ee16e2c0f4
joooii <3098k@naver.com>
2025-12-11 10:39:31 +0900

    FEAT: Signup API 연동 #6

--- src/actions/auth/SignUpServerAction.ts
+++ src/actions/auth/SignUpServerAction.ts
@@ -1,16 +1,15 @@
 "use server";
 
-import { redirect } from "next/navigation";
-
 export default async function signupAction(formData: FormData) {
   const name = formData.get("name");
-  const id = formData.get("id");
+  const loginId = formData.get("id");
   const password = formData.get("password");
   const confirmPassword = formData.get("confirmPassword");
-  const birth = formData.get("birth");
+  const birthRaw = formData.get("birth") as string;
+  const birth = new Date(birthRaw).toISOString().split("T")[0];
 
   // 1) 빈 값 체크
-  if (!name || !id || !password || !confirmPassword || !birth) {
+  if (!name || !loginId || !password || !confirmPassword || !birth) {
     throw new Error("모든 값을 입력해주세요.");
   }
   // 2) 비밀번호 길이 검증
@@ -22,5 +21,38 @@ export default async function signupAction(formData: FormData) {
     throw new Error("비밀번호가 일치하지 않습니다.");
   }
 
-  redirect("/login");
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/signup`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name,
+          loginId,
+          password,
+          passwordCheck: confirmPassword,
+          birth,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const data = await response.json();
+      return {
+        success: false,
+        message: data?.message || "회원가입 실패",
+      };
+    } else {
+      return {
+        success: true,
+      };
+    }
+  } catch (error) {
+    console.error("회원가입 에러", error);
+    return {
+      success: false,
+      message: "회원가입 처리 중 오류가 발생했습니다.",
+    };
+  }
 }
--- src/components/auth/signup/SignUpForm.tsx
+++ src/components/auth/signup/SignUpForm.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import Input from "@/components/ui/Input";
 import { Eye, EyeOff } from "lucide-react";
 import ActionButton from "@/components/ui/ActionButton";
 import FitlogLogo from "@/components/ui/FitLogLogo";
 import useSignUp from "@/hooks/useSignUp";
 import signupAction from "@/actions/auth/SignUpServerAction";
+import { useRouter } from "next/navigation";
 
 export default function SignUpForm() {
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [showConfirmPassword, setShowConfirmPassword] =
     useState<boolean>(false);
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string>("");
+  const router = useRouter();
 
   const {
     signupFormData,
@@ -24,132 +28,156 @@ export default function SignUpForm() {
   } = useSignUp();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    if (!isFormValid) {
-      e.preventDefault();
-    }
-    console.log("회원가입 성공", signupFormData);
+    e.preventDefault();
+    setError("");
+
+    if (!isFormValid) return;
+
+    const formData = new FormData(e.currentTarget);
+
+    startTransition(async () => {
+      const result = await signupAction(formData);
+
+      if (!result.success) {
+        setError(result.message || "회원가입에 실패했습니다.");
+        return;
+      }
+
+      router.push("/login");
+    });
   };
 
   return (
-    <form action={signupAction} onSubmit={handleSubmit}>
-      <div className="flex h-194 w-96 flex-col items-center rounded-xl bg-white border-fitlog-beige border shadow-fitlog-form">
-        <div className="flex flex-row items-center justify-center py-14">
-          <FitlogLogo size={48} />
-          <p className="text-fitlog-500 text-4xl font-extrabold ml-4">FitLog</p>
-        </div>
+    <>
+      {error && (
+        <p className="text-red-500 text-sm mb-3 text-center">{error}</p>
+      )}
+      <form onSubmit={handleSubmit}>
+        <div className="flex h-194 w-96 flex-col items-center rounded-xl bg-white border-fitlog-beige border shadow-fitlog-form">
+          <div className="flex flex-row items-center justify-center py-14">
+            <FitlogLogo size={48} />
+            <p className="text-fitlog-500 text-4xl font-extrabold ml-4">
+              FitLog
+            </p>
+          </div>
 
-        <div className="flex w-full flex-col px-8">
-          <p className="mb-2 ml-1">이름</p>
-          <Input
-            type="text"
-            placeholder="이름"
-            className="w-full px-3"
-            required
-            name="name"
-            onChange={handleSignUpChange}
-          />
-        </div>
-        <div className="mt-6 flex w-full flex-col px-8">
-          <p className="mb-2 ml-1">아이디</p>
-          <Input
-            type="text"
-            placeholder="아이디"
-            className="w-full px-3"
-            required
-            name="id"
-            onChange={handleSignUpChange}
-          />
-        </div>
-        <div className="mt-6 flex w-full flex-col px-8">
-          <p className="mb-2 ml-1">비밀번호</p>
-          <div className="relative w-full">
+          <div className="flex w-full flex-col px-8">
+            <p className="mb-2 ml-1">이름</p>
             <Input
-              type={showPassword ? "text" : "password"}
-              placeholder="비밀번호"
-              className="w-full pr-10 px-3"
+              type="text"
+              placeholder="이름"
+              className="w-full px-3"
               required
-              name="password"
+              name="name"
               onChange={handleSignUpChange}
             />
-            {showPassword ? (
-              <EyeOff
-                onClick={() => setShowPassword(false)}
-                className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
-              />
-            ) : (
-              <Eye
-                onClick={() => setShowPassword(true)}
-                className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
-              />
-            )}
           </div>
-          {signupFormData.password.length > 0 && !isPasswordLengthValid && (
-            <p className="text-red-500 text-xs mt-1 ml-1">
-              * 비밀번호는 8자 이상이어야 합니다.
-            </p>
-          )}
-        </div>
-        <div className="mt-6 flex w-full flex-col px-8">
-          <p className="mb-2 ml-1">비밀번호 확인</p>
-          <div className="relative w-full">
+          <div className="mt-6 flex w-full flex-col px-8">
+            <p className="mb-2 ml-1">아이디</p>
             <Input
-              type={showConfirmPassword ? "text" : "password"}
-              placeholder="비밀번호 확인"
-              className="w-full pr-10 px-3"
+              type="text"
+              placeholder="아이디"
+              className="w-full px-3"
               required
-              name="confirmPassword"
+              name="id"
               onChange={handleSignUpChange}
             />
-            {showConfirmPassword ? (
-              <EyeOff
-                onClick={() => setShowConfirmPassword(false)}
-                className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
+          </div>
+          <div className="mt-6 flex w-full flex-col px-8">
+            <p className="mb-2 ml-1">비밀번호</p>
+            <div className="relative w-full">
+              <Input
+                type={showPassword ? "text" : "password"}
+                placeholder="비밀번호"
+                className="w-full pr-10 px-3"
+                required
+                name="password"
+                onChange={handleSignUpChange}
               />
-            ) : (
-              <Eye
-                onClick={() => setShowConfirmPassword(true)}
-                className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
-              />
+              {showPassword ? (
+                <EyeOff
+                  onClick={() => setShowPassword(false)}
+                  className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
+                />
+              ) : (
+                <Eye
+                  onClick={() => setShowPassword(true)}
+                  className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
+                />
+              )}
+            </div>
+            {signupFormData.password.length > 0 && !isPasswordLengthValid && (
+              <p className="text-red-500 text-xs mt-1 ml-1">
+                * 비밀번호는 8자 이상이어야 합니다.
+              </p>
             )}
           </div>
-          {signupFormData.confirmPassword.length > 0 && !isSamePassword && (
-            <p className="text-red-500 text-xs mt-1 ml-1">
-              * 비밀번호가 일치하지 않습니다.
-            </p>
-          )}
-        </div>
+          <div className="mt-6 flex w-full flex-col px-8">
+            <p className="mb-2 ml-1">비밀번호 확인</p>
+            <div className="relative w-full">
+              <Input
+                type={showConfirmPassword ? "text" : "password"}
+                placeholder="비밀번호 확인"
+                className="w-full pr-10 px-3"
+                required
+                name="confirmPassword"
+                onChange={handleSignUpChange}
+              />
+              {showConfirmPassword ? (
+                <EyeOff
+                  onClick={() => setShowConfirmPassword(false)}
+                  className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
+                />
+              ) : (
+                <Eye
+                  onClick={() => setShowConfirmPassword(true)}
+                  className="text-fitlog-beige absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer stroke-1 hover:text-[#d7d0ce]"
+                />
+              )}
+            </div>
+            {signupFormData.confirmPassword.length > 0 && !isSamePassword && (
+              <p className="text-red-500 text-xs mt-1 ml-1">
+                * 비밀번호가 일치하지 않습니다.
+              </p>
+            )}
+          </div>
 
-        <div className="mt-6  flex w-full flex-col px-8">
-          <p className="mb-2 ml-1">생년월일</p>
-          <Input
-            type="date"
-            placeholder="YYYY.MM.DD"
-            className="w-full px-3"
-            required
-            name="birth"
-            onChange={handleSignUpChange}
-          />
-          {!signupFormData.birth && !isBirthValid && (
-            <p className="text-red-500 text-sm mt-1">
-              * 올바른 생년월일을 입력해주세요.
-            </p>
-          )}
-        </div>
-        <div className="mt-7 flex w-full flex-col px-8">
-          <ActionButton className="px-3 py-2.5" type="submit">
-            회원가입
-          </ActionButton>
-          <div className="mt-5 flex flex-row justify-center text-sm">
-            <p>이미 계정이 있으신가요?</p>
-            <Link
-              href={"/login"}
-              className="text-fitlog-500 hover:text-fitlog-700 ml-3 font-bold"
+          <div className="mt-6  flex w-full flex-col px-8">
+            <p className="mb-2 ml-1">생년월일</p>
+            <Input
+              type="date"
+              placeholder="YYYY.MM.DD"
+              className="w-full px-3"
+              required
+              name="birth"
+              onChange={handleSignUpChange}
+            />
+            {!signupFormData.birth && !isBirthValid && (
+              <p className="text-red-500 text-sm mt-1">
+                * 올바른 생년월일을 입력해주세요.
+              </p>
+            )}
+          </div>
+          <div className="mt-7 flex w-full flex-col px-8">
+            <ActionButton
+              disabled={isPending}
+              className="px-3 py-2.5"
+              type="submit"
             >
-              로그인
-            </Link>
+              {isPending ? "회원가입 중..." : "회원가입"}
+            </ActionButton>
+            <div className="mt-5 flex flex-row justify-center text-sm">
+              <p>이미 계정이 있으신가요?</p>
+              <Link
+                href={"/login"}
+                className="text-fitlog-500 hover:text-fitlog-700 ml-3 font-bold"
+              >
+                로그인
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
