f7f880fe8de59bbbe65f22c6ab3dbece8d615820
joonhyong <dbshwns0528@naver.com>
2025-12-17 12:58:49 +0900

    FEAT: src/types/exercise.ts 생성 -> 백엔드 ExerciseRespinseDTO와 매칭

---
+++ src/types/exercise.ts
@@ -0,0 +1,20 @@
+export interface ExerciseItem {
+  todoId: number;
+  exerciseId: number;
+  exerciseName: string;
+  setsNumber: number;
+  repsTarget: number;
+  weight: number | null;
+  restTime: number | null;
+  isCompleted: boolean;
+  caloriesPerRep?: number;
+  burnedCalories?: number;
+}
+
+export interface ExerciseResponse {
+  date: string;
+  isDone: boolean;
+  exercises: ExerciseItem[];
+  totalCalories: number;
+  message: string;
+}
