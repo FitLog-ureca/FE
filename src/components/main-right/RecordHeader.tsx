43fa58051c2eae595fe1fc4ef31eef489c56cef6
joonhyong <dbshwns0528@naver.com>
2025-12-21 22:43:19 +0900

    FEAT: 칼로리 참고 문구 추가

--- src/components/main-right/RecordHeader.tsx
+++ src/components/main-right/RecordHeader.tsx
@@ -39,11 +39,25 @@ export default function RecordHeader({ selectedDate, totalCalories, isDone }: Re
         </div>
       </section>
 
-      <section className="w-full flex gap-2 rounded-xl p-4 border border-fitlog-beige bg-white shadow-fitlog-btn-sm">
-        <span className="font-semibold">총 소모 칼로리</span>
-        <span>-</span>
-        <span className="font-semibold text-fitlog-500">{totalCalories.toFixed(1)}</span>
-        <span>kcal</span>
+      <section className="w-full flex justify-between items-center gap-2 rounded-xl p-4 border border-fitlog-beige bg-white shadow-fitlog-btn-sm relative">
+        <div className="flex gap-2">
+          <span className="font-semibold">총 소모 칼로리</span>
+          <span>-</span>
+          <span className="font-semibold text-fitlog-500">{totalCalories.toFixed(1)}</span>
+          <span>kcal</span>
+        </div>
+        <div className="flex gap-1">
+          <div className="flex flex-col">
+            <span className="text-xs text-gray-400">※</span>
+            <span className="">{""}</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xs text-gray-400">해당 칼로리는 추정치입니다.</span>
+            <span className="text-xs text-gray-400">
+              개인의 체중·운동 강도에 따라 차이가 있을 수 있습니다.
+            </span>
+          </div>
+        </div>
       </section>
     </div>
   );
